e84a44e1fd99f509edb166c7ff0ac332a6fd0987
Ajeet Gathala <ajeetgathala@gmail.com>
2019-08-07 18:22:07 +0530

    Added notification from firebase

--- app/src/main/java/com/example/notificationsapmle/MessagingService.java
+++ app/src/main/java/com/example/notificationsapmle/MessagingService.java
@@ -3,7 +3,6 @@ package com.example.notificationsapmle;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
@@ -12,14 +11,16 @@ import android.widget.RemoteViews;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
-public class Receiver extends BroadcastReceiver {
-    String CHANNEL_ID = "mynotification";
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
+public class MessagingService extends FirebaseMessagingService {
+    String CHANNEL_ID = "firebasenotification";
+    Context context=MessagingService.this;
     @Override
-    public void onReceive(Context context, Intent intent) {
-        showNotification(context, "Times Up", "Time has come");
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        showNotification(context,"Firebase Notification",remoteMessage.getNotification().getBody());
     }
-
     public void showNotification(Context context, String title, String msg) {
         RemoteViews notificationLayout = new RemoteViews(context.getPackageName(), R.layout.notification_layout);
         RemoteViews notificationLayoutExpanded = new RemoteViews(context.getPackageName(), R.layout.notification_layout);
